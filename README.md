1133e018b812393328f6b5b254547831c457f3fd
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-06-12 18:25:47 -0400

    WIP: Setup requirements, prelim conda recipe, and docs

---
+++ README.md
@@ -0,0 +1,3 @@
+# lume-model
+
+Lume-model contains the variable classes required for running LUME models.
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from os import path, environ
+
+cur_dir = path.abspath(path.dirname(__file__))
+
+# parse requirements
+with open(path.join(cur_dir, "requirements.txt"), "r") as f:
+    requirements = f.read().split()
+
+# set up additional dev requirements
+dev_requirements = []
+with open(path.join(cur_dir, "dev-requirements.txt"), "r") as f:
+    dev_requirements = f.read().split()
+
+
+setup(
+    name="lume-model",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=requirements,
+    # set up development requirements
+    extras_require={"dev": dev_requirements},
+    url="https://github.com/slaclab/lume-model",
+    include_package_data=True,
+    python_requires=">=3.7",
+)
