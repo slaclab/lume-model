12ed0cd5fa00c34f0dede4fbd05a68bf3948ad0b
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-07-20 17:46:06 -0400

    MNT: Make shape declaration mandatory

--- lume_model/variables.py
+++ lume_model/variables.py
@@ -1,3 +1,7 @@
+"""
+This module contains lume-model variable definitions.
+"""
+
 import numpy as np
 from enum import Enum
 import logging
@@ -6,61 +10,6 @@ from pydantic import BaseModel, Field, validator
 from pydantic.generics import GenericModel
 
 logger = logging.getLogger(__name__)
-
-
-class PropertyBaseModel(GenericModel):
-    """
-    Generic base class used for the Variables. This extends the pydantic GenericModel to serialize properties.
-
-
-    Notes
-    -----
-    Workaround for serializing properties with pydantic until
-    https://github.com/samuelcolvin/pydantic/issues/935
-    is solved. This solution is referenced in the issue.
-    """
-
-    @classmethod
-    def get_properties(cls):
-        return [
-            prop
-            for prop in dir(cls)
-            if isinstance(getattr(cls, prop), property)
-            and prop not in ("__values__", "fields")
-        ]
-
-    def dict(
-        self,
-        *,
-        include: Union["AbstractSetIntStr", "MappingIntStrAny"] = None,
-        exclude: Union["AbstractSetIntStr", "MappingIntStrAny"] = None,
-        by_alias: bool = False,
-        skip_defaults: bool = None,
-        exclude_unset: bool = False,
-        exclude_defaults: bool = False,
-        exclude_none: bool = False,
-    ) -> "DictStrAny":
-        attribs = super().dict(
-            include=include,
-            exclude=exclude,
-            by_alias=by_alias,
-            skip_defaults=skip_defaults,
-            exclude_unset=exclude_unset,
-            exclude_defaults=exclude_defaults,
-            exclude_none=exclude_none,
-        )
-        props = self.get_properties()
-        # Include and exclude properties
-        if include:
-            props = [prop for prop in props if prop in include]
-        if exclude:
-            props = [prop for prop in props if prop not in exclude]
-
-        # Update the attribute dict with the properties
-        if props:
-            attribs.update({prop: getattr(self, prop) for prop in props})
-
-        return attribs
 
 
 class NumpyNDArray(np.ndarray):
@@ -85,13 +34,9 @@ class Image(np.ndarray):
     """
     Custom type validator for image array.
 
+    Note:
+        This should be expanded to check for color images.
 
-    Notes
-    -----
-    This should be expanded to check for color images.
-
-
-    #dw, dh
     """
 
     @classmethod
@@ -118,28 +63,22 @@ class Image(np.ndarray):
 Value = TypeVar("Value")
 
 
-class Variable(PropertyBaseModel, Generic[Value]):
+class Variable(BaseModel, Generic[Value]):
     """
     Minimum requirements for a Variable
 
-    Attributes
-    ----------
-    name: str
-        Name of the variable
+    Attributes:
+        name (str): Name of the variable.
 
-    default: Value, optional
-        Default value assigned to the variable
+        default (Value, optional):  Default value assigned to the variable
 
-    units: str, optional
-        Units associated with the value
-
-    precision: int
-        Precision to use for the value
+        precision (int, optional): Precision to use for the value
 
     """
 
     name: str = Field(...)  # name required
     default: Optional[Value]  # default optionally required
+    precision: Optional[int] = 8
 
     class Config:
         allow_population_by_field_name = True  # do not use alias only-init
@@ -149,13 +88,16 @@ class InputVariable(Variable, Generic[Value]):
     """
     Base generic class for input variables.
 
-    Attributes
-    ----------
-    value: Value
-        Value assigned to variable
+    Attributes:
+        name (str): Name of the variable.
 
-    value_range: list
-        Acceptable range for value
+        default (Value, optional):  Default value assigned to the variable
+
+        precision (int, optional): Precision to use for the value
+
+        value (Value): Value assigned to variable
+
+        value_range (list): Acceptable range for value
 
     """
 
@@ -167,13 +109,16 @@ class OutputVariable(Variable, Generic[Value]):
     """
     Base generic class for output variables. Value and range assignment are optional.
 
-    Attributes
-    ----------
-    value: Value, optional
-        Value  assigned to variable
+    Attributes:
+        name (str): Name of the variable.
 
-    value_range: list, optional
-        Acceptable range for value
+        default (Value, optional):  Default value assigned to the variable
+
+        precision (int, optional): Precision to use for the value
+
+        value (Value, optional): Value assigned to variable
+
+        value_range (list, optional): Acceptable range for value
 
     """
 
@@ -181,27 +126,30 @@ class OutputVariable(Variable, Generic[Value]):
     value_range: Optional[list] = Field(alias="range")
 
 
-class NDVariableBase:
-    """
-    Holds properties associated with numpy array variables.
-
-    Attributes
-    ----------
-    shape: tuple
-        Shape of the numpy n-dimensional array
-
-    """
-
-    @property
-    def shape(self) -> tuple:
-        return self.value.shape
-
-
 class ImageVariable(BaseModel):
+    """
+    Base class used for constructing an image variable.
+
+    Attributes:
+        variable_type (tuple="image): Indicates image variable
+
+        axis_labels (List[str]): Labels to use for rendering axes
+
+        axis_units (Lsit[str]): Units to use for rendering axes labels
+
+        x_min (float): Minimum x value of image
+
+        x_max (float): Maximum x value of image
+
+        y_min (float): Minimum y value of image
+
+        y_max (float): Maximum y value of image
+    """
+
     variable_type = "image"
     axis_labels: List[str]
+    shape: tuple
     axis_units: List[str] = None
-    precision: int = 8
     x_min: float = None
     x_max: float = None
     y_min: float = None
@@ -209,23 +157,36 @@ class ImageVariable(BaseModel):
 
 
 class ScalarVariable:
+    """
+    Base class used for constructing a scalar variable.
+
+    Attributes:
+        variable_type (tuple="scalar"): Type of variable
+
+        units (str): Units associated with scalar value
+    """
+
     variable_type = "scalar"
     units: Optional[str]  # required for some output displays
-    precision: int = 8
 
 
-class ImageInputVariable(InputVariable[Image], NDVariableBase, ImageVariable):
+class ImageInputVariable(InputVariable[Image], ImageVariable):
     """
     Class composition of image input, and numpy array base class.
+
+    Attributes:
 
     """
 
     pass
 
 
-class ImageOutputVariable(OutputVariable[Image], NDVariableBase, ImageVariable):
+class ImageOutputVariable(OutputVariable[Image], ImageVariable):
     """
     Class composition of image output, and numpy array base class.
+
+    Attributes:
+
 
     """
 
@@ -243,6 +204,9 @@ class ScalarInputVariable(InputVariable[float], ScalarVariable):
 class ScalarOutputVariable(OutputVariable[float], ScalarVariable):
     """
     Class composition of scalar output and scalar base.
+
+    Attributes:
+
     """
 
     pass
