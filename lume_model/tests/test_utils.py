300b4f2c45f5a363961ce3c2206c833c9b3df81a
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-07-22 09:47:05 -0400

    MNT: Update util save function and tests

--- lume_model/tests/test_utils.py
+++ lume_model/tests/test_utils.py
@@ -1,9 +1,10 @@
 from lume_model import utils
+import os
 from lume_model.models import SurrogateModel
 from lume_model.variables import ScalarInputVariable, ScalarOutputVariable
 
 
-class ExampleModel(SurrogateModel):
+def test_save():
     input_variables = {
         "input1": ScalarInputVariable(name="input1", default=1, range=[0.0, 5.0]),
         "input2": ScalarInputVariable(name="input2", default=2, range=[0.0, 5.0]),
@@ -14,22 +15,7 @@ class ExampleModel(SurrogateModel):
         "output2": ScalarOutputVariable(name="output2"),
     }
 
-    def evaluate(self, input_variables):
-
-        self.input_variables = {variable.name: variable for variable in input_variables}
-
-        self.output_variables["output1"].value = (
-            self.input_variables["input1"].value * 2
-        )
-        self.output_variables["output2"].value = (
-            self.input_variables["input2"].value * 2
-        )
-
-        # return inputs * 2
-        return list(self.output_variables.values())
-
-
-def test_save():
     file_name = "test_variables.pickle"
-    utils.save_variables(ExampleModel, file_name)
+    utils.save_variables(input_variables, output_variables, file_name)
     utils.load_variables(file_name)
+    os.remove(file_name)
