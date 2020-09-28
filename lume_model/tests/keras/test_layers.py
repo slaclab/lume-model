c728ccbbbd32c4a4598647834007aefa0bd23ebc
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-09-28 10:18:21 -0700

    TST: Set up keras tests

---
+++ lume_model/tests/keras/test_layers.py
@@ -0,0 +1,36 @@
+import pytest
+from lume_model.keras.layers import ScaleLayer, UnscaleLayer, UnscaleImgLayer
+
+
+# test value and failed initialization with characters
+@pytest.mark.parametrize(
+    "offset,scale,lower,upper",
+    [
+        (1, 2, 0, 1),
+        (5, 4, -1, 1),
+        pytest.param("t", "e", "s", "t", marks=pytest.mark.xfail),
+    ],
+)
+def test_scale_layer(offset, scale, lower, upper):
+    scale_layer = ScaleLayer(offset, scale, lower, upper)
+
+
+# test value and failed initialization with characters
+@pytest.mark.parametrize(
+    "offset,scale,lower,upper",
+    [
+        (1, 2, 0, 1),
+        (5, 4, -1, 1),
+        pytest.param("t", "e", "s", "t", marks=pytest.mark.xfail),
+    ],
+)
+def test_unscale_layer(offset, scale, lower, upper):
+    unscale_layer = UnscaleLayer(offset, scale, lower, upper)
+
+
+# test value and failed initialization with characters
+@pytest.mark.parametrize(
+    "offset,scale", [(1, 2), (5, 4), pytest.param("t", "e", marks=pytest.mark.xfail),],
+)
+def test_unscale_image_layer(offset, scale):
+    unscale_layer = UnscaleImgLayer(offset, scale)
