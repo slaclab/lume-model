bea1718a10ee42a96cef6b0cf2ea11a1ffe0fc37
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-10-01 09:16:06 -0700

    DEP: Deprecate the custom IrisModel class

--- examples/iris_model.py
+++ examples/iris_model.py
@@ -1,29 +1,7 @@
 """
 Adaptation of tensorflow tutorial: https://www.tensorflow.org/tutorials/estimator/premade
 """
+from lume_model.utils import model_from_yaml
 
-from lume_model.keras import BaseModel
-from lume_model.variables import ScalarInputVariable, ScalarOutputVariable
-import numpy as np
-
-
-class IrisModel(BaseModel):
-    def format_input(self, input_dictionary):
-        """Formats input to be fed into model
-        """
-        vector = np.array(
-            [
-                input_dictionary["SepalLength"],
-                input_dictionary["SepalWidth"],
-                input_dictionary["PetalLength"],
-                input_dictionary["PetalWidth"],
-            ]
-        ).reshape(1, 4)
-
-        return vector
-
-    def parse_output(self, model_output):
-        """Parses model output to create dictionary variable name -> value
-        """
-        softmax_output = list(model_output[0])
-        return {"Species": softmax_output.index(max(softmax_output))}
+model = model_from_yaml("examples/files/iris_config.yaml")
+model.random_evaluate()
