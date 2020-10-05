0cc1c9fe9437adb0d7b96fb4db3b87d7f22b16fb
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-10-05 09:18:34 -0700

    MNT: Update examples to use file

--- examples/iris_model.py
+++ examples/iris_model.py
@@ -3,5 +3,7 @@ Adaptation of tensorflow tutorial: https://www.tensorflow.org/tutorials/estimato
 """
 from lume_model.utils import model_from_yaml
 
-model = model_from_yaml("examples/files/iris_config.yaml")
+with open("examples/files/iris_config.yaml", "r") as f:
+    model = model_from_yaml(f)
+
 model.random_evaluate()
