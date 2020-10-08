2240872749adcea39e1a1d0a0ccca9f8dfc06f01
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-10-08 15:06:28 -0700

    TST: Use parametrize marker for variable tests

--- lume_model/tests/test_variables.py
+++ lume_model/tests/test_variables.py
@@ -9,28 +9,29 @@ from lume_model.variables import (
 )
 
 
-def test_input_scalar_variable():
+@pytest.mark.parametrize(
+    "variable_name,default,value_range",
+    [
+        ("test", 0.1, [0.1, 2]),
+        pytest.param("test", np.array([1, 2, 3, 4]), [0, 1], marks=pytest.mark.xfail),
+    ],
+)
+def test_input_scalar_variable(variable_name, default, value_range):
     # test correctly typed
-    ScalarInputVariable(name="test", default=0.1, value_range=[1, 2])
-
-    # test incorrect value type
-    with pytest.raises(ValidationError):
-        ScalarInputVariable(
-            name="test", default=np.array([1, 2, 3, 4]), value_range=[1, 2]
-        )
+    ScalarInputVariable(name=variable_name, default=default, value_range=value_range)
 
     # test missing name
     with pytest.raises(ValidationError):
-        ScalarInputVariable(default=0.1, value_range=[1, 2])
+        ScalarInputVariable(default=default, value_range=value_range)
 
     # test missing default
     with pytest.raises(ValidationError):
-        ScalarInputVariable(name="test", value_range=[1, 2])
+        ScalarInputVariable(name=variable_name, value_range=value_range)
 
     # test missing range
     with pytest.raises(ValidationError):
         ScalarInputVariable(
-            name="test", default=0.1,
+            name=variable_name, default=default,
         )
 
 
@@ -57,153 +58,129 @@ def test_constant_input_scalar_variable(
         variable.default = assignment
 
 
-def test_output_scalar_variable():
+@pytest.mark.parametrize(
+    "variable_name,default,value_range",
+    [
+        ("test", 0.1, [0.1, 2]),
+        pytest.param("test", np.array([1, 2, 3, 4]), [0, 1], marks=pytest.mark.xfail),
+    ],
+)
+def test_output_scalar_variable(variable_name, default, value_range):
     # test correctly typed
-    ScalarOutputVariable(name="test", default=0.1, value_range=[1, 2])
-
-    # test incorrect value type
-    with pytest.raises(ValidationError):
-        ScalarOutputVariable(
-            name="test", default=np.array([1, 2, 3, 4]), value_range=[1, 2]
-        )
+    ScalarOutputVariable(name=variable_name, default=default, value_range=value_range)
 
     # test missing name
     with pytest.raises(ValidationError):
-        ScalarOutputVariable(default=0.1, value_range=[1, 2])
+        ScalarOutputVariable(default=default, value_range=value_range)
 
     # test missing value
-    ScalarOutputVariable(name="test", value_range=[1, 2])
+    ScalarOutputVariable(name=variable_name, value_range=value_range)
 
     # test missing range
     ScalarOutputVariable(
-        name="test", default=0.1,
+        name=variable_name, default=default,
     )
 
 
-def test_input_image_variable():
+@pytest.mark.parametrize(
+    "variable_name,default,value_range,axis_labels,x_min,y_min,x_max,y_max",
+    [
+        ("test", np.array([[1, 2,], [3, 4]]), [0, 1], ["x", "y"], 0, 0, 1, 1),
+        pytest.param(
+            "test", 1.0, [0, 1], ["x", "y"], 0, 0, 1, 1, marks=pytest.mark.xfail
+        ),
+    ],
+)
+def test_input_image_variable(
+    variable_name, default, value_range, axis_labels, x_min, y_min, x_max, y_max
+):
     # test correctly typed
-    test_array = np.array([[1, 2,], [3, 4]])
     ImageInputVariable(
-        name="test",
-        default=test_array,
-        value_range=[1, 10],
-        axis_labels=["count_1", "count_2"],
-        x_min=0,
-        y_min=0,
-        x_max=5,
-        y_max=5,
+        name=variable_name,
+        default=default,
+        value_range=value_range,
+        axis_labels=axis_labels,
+        x_min=x_min,
+        y_min=y_min,
+        x_max=x_max,
+        y_max=y_max,
     )
-
-    # test image value constraints
-    with pytest.raises(ValidationError):
-        ImageInputVariable(
-            name="test",
-            default=np.array([1, 2, 3, 4]),
-            value_range=[1, 10],
-            axis_labels=["count_1", "count_2"],
-            x_min=0,
-            y_min=0,
-            x_max=5,
-            y_max=5,
-        )
 
     # test missing name
     with pytest.raises(ValidationError):
         ImageInputVariable(
-            default=test_array,
-            value_range=[1, 10],
-            axis_labels=["count_1", "count_2"],
-            x_min=0,
-            y_min=0,
-            x_max=5,
-            y_max=5,
+            default=default,
+            value_range=value_range,
+            axis_labels=axis_labels,
+            x_min=x_min,
+            y_min=y_min,
+            x_max=x_max,
+            y_max=y_max,
         )
 
     # test missing value
     with pytest.raises(ValidationError):
         ImageInputVariable(
-            name="test",
-            value_range=[1, 10],
-            axis_labels=["count_1", "count_2"],
-            x_min=0,
-            y_min=0,
-            x_max=5,
-            y_max=5,
+            name=variable_name,
+            value_range=value_range,
+            axis_labels=axis_labels,
+            x_min=x_min,
+            y_min=y_min,
+            x_max=x_max,
+            y_max=y_max,
         )
 
     # test missing range
     with pytest.raises(ValidationError):
         ImageInputVariable(
-            name="test",
-            default=np.array([[1, 2,], [3, 4]]),
-            axis_labels=["count_1", "count_2"],
-            x_min=0,
-            y_min=0,
-            x_max=5,
-            y_max=5,
+            name=variable_name,
+            default=default,
+            axis_labels=axis_labels,
+            x_min=x_min,
+            y_min=y_min,
+            x_max=x_max,
+            y_max=y_max,
         )
 
     # test missing axis labels
     with pytest.raises(ValidationError):
         ImageInputVariable(
-            name="test",
-            default=np.array([[1, 2,], [3, 4]]),
-            shape=test_array.shape,
-            value_range=[1, 10],
-            x_min=0,
-            y_min=0,
-            x_max=5,
-            y_max=5,
+            name=variable_name,
+            default=default,
+            value_range=value_range,
+            x_min=x_min,
+            y_min=y_min,
+            x_max=x_max,
+            y_max=y_max,
         )
 
 
-def test_output_image_variable():
-    # test correctly typed
-    test_array = np.array([[1, 2,], [3, 4]])
-
+@pytest.mark.parametrize(
+    "variable_name,default,axis_labels",
+    [
+        ("test", np.array([[1, 2,], [3, 4]]), ["x", "y"],),
+        pytest.param("test", 1.0, ["x", "y"], marks=pytest.mark.xfail),
+    ],
+)
+def test_output_image_variable(variable_name, default, axis_labels):
+    shape = default.shape
     ImageOutputVariable(
-        name="test",
-        default=test_array,
-        shape=test_array.shape,
-        axis_labels=["count_1", "count_2"],
+        name=variable_name, default=default, shape=shape, axis_labels=axis_labels,
     )
-
-    # test dim of image value must = 2
-    with pytest.raises(ValidationError):
-        ImageOutputVariable(
-            name="test",
-            default=np.array([1, 2, 3, 4]),
-            shape=test_array.shape,
-            axis_labels=["count_1", "count_2"],
-        )
 
     # test missing name
     with pytest.raises(ValidationError):
         ImageOutputVariable(
-            default=test_array,
-            shape=test_array.shape,
-            axis_labels=["count_1", "count_2"],
+            default=default, shape=shape, axis_labels=axis_labels,
         )
 
     # test missing axis labels
     with pytest.raises(ValidationError):
         ImageOutputVariable(
-            name="test", default=test_array,
+            name=variable_name, default=default,
         )
 
     # test missing value
     ImageOutputVariable(
-        name="test", axis_labels=["count_1", "count_2"],
+        name=variable_name, axis_labels=axis_labels,
     )
-
-    ImageOutputVariable(
-        name="test",
-        value=np.array([[1, 2,], [3, 4]]),
-        shape=test_array.shape,
-        axis_labels=["count_1", "count_2"],
-    )
-
-
-if __name__ == "__main__":
-
-    test_constant_input_scalar_variable()
