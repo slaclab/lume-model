e881cb1ddfa933f5fcd744ba5575cd86efbc60a3
Tobias Boltz <tboltz@slac.stanford.edu>
2023-11-09 12:04:52 -0800

    Update examples

--- examples/iris_model.py
+++
@@ -1,9 +0,0 @@
-"""
-Adaptation of tensorflow tutorial: https://www.tensorflow.org/tutorials/estimator/premade
-"""
-from lume_model.utils import model_from_yaml
-
-with open("examples/files/iris_config.yml", "r") as f:
-    model = model_from_yaml(f)
-
-model.random_evaluate()
