f35ee702b46cc7eb6cc962cabc5c14ecd6c1f7db
Sara A. Miskovich <saramiskovich@gmail.com>
2025-02-11 20:02:21 -0800

    adjust dist methods

--- lume_model/models/gp_model.py
+++ lume_model/models/gp_model.py
@@ -1,7 +1,9 @@
 import logging
 from typing import Union
 
+import numpy as np
 import torch
+from torch.distributions import Distribution as  TDistribution
 from gpytorch.models import ExactGP
 from botorch.models.gpytorch import BatchedMultiOutputGPyTorchModel
 from botorch.fit import fit_gpytorch_mll
@@ -19,30 +21,11 @@ class GPModel(LUMEBaseModel):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    # TODO: add properties
+    # TODO: add validation
 
-    def mean(self, x):
-        """Returns the mean of the posterior distribution"""
-        self.model.eval()
-        with torch.no_grad():
-            posterior = self.model.posterior(x)
-            mean = posterior.mean
-        return mean
-
-    def variance(self, x):
-        """Returns the variance of the posterior distribution"""
-        self.model.eval()
-        with torch.no_grad():
-            posterior = self.model.posterior(x)
-            variance = posterior.variance
-        return variance
-
-    def sample(self, x):
-        """Returns samples from the posterior distribution"""
-        self.model.eval()
-        with torch.no_grad():
-            posterior = self.model.posterior(x)
-            samples = posterior.rsample()
-        return samples
+    def likelihood(self):
+        return self.model.likelihood
 
     def mll(self, x, y):
         """ Returns the marginal log-likelihood value"""
@@ -50,17 +33,92 @@ class GPModel(LUMEBaseModel):
         mll = ExactMarginalLogLikelihood(self.model.likelihood, self.model)
         return mll(self.model(x), y).item()
 
-    def log_likelihood(self, x):
-        """Returns the likelihood values for predictions from the likelihood object"""
+    def _posterior(self, x):
         self.model.eval()
-        self.model.likelihood.eval()
-        with torch.no_grad():
-            observed_pred = self.model.likelihood(self.model(x))
+        posterior = self.model.posterior(x)
+        return posterior
 
-        return  observed_pred.mean, observed_pred.variance
+    def _evaluate(self, x): # make input dict and output dict
+        # transform
 
-    def _evaluate(self, x):
-        self.model.eval()
-        mean_x = self.mean_module(x)
-        covar_x = self.covar_module(x)
-        return MultivariateNormal(mean_x, covar_x)
+        # evaluate
+        posterior = self._posterior(x)
+
+        # wrap the distribution in a torch distribution
+        distribution = self.get_distribution(posterior)
+
+        # untransform and prepare output
+
+        return distribution
+
+    def get_distribution(self, posterior):
+        if isinstance(posterior.distribution, TDistribution):
+            return posterior.distribution
+        else:
+            # wrap the distribution in a torch distribution
+            return TorchDistributionWrapper(posterior.distribution)
+
+
+class TorchDistributionWrapper(TDistribution):
+    """Wraps any distribution to provide a torch.distributions-like interface."""
+    def __init__(self, custom_dist):
+        """
+        Args:
+            custom_dist: An instance of a custom distribution with methods like mean, variance,
+                          log_prob, sample, and rsample.
+        """
+        super().__init__()
+        self.custom_dist = custom_dist
+
+    @property
+    def mean(self):
+        """Return the mean of the custom distribution."""
+        if hasattr(self.custom_dist, 'mean'):
+            return self.custom_dist.mean()
+        raise NotImplementedError("Mean method is not implemented for this distribution.")
+
+    @property
+    def variance(self):
+        """Return the variance of the custom distribution."""
+        if hasattr(self.custom_dist, 'variance'):
+            return self.custom_dist.variance()
+        elif hasattr(self.custom_dist, 'var'):
+            return self.custom_dist.var(value)
+        elif hasattr(self.custom_dist, 'cov'):
+            # Scipy distributions have cov method
+            return np.diagonal(self.custom_dist.cov())
+        elif hasattr(self.custom_dist, 'covariance_matrix'):
+            return np.diagonal(self.custom_dist.covariance_matrix())
+        raise NotImplementedError("Variance method is not implemented for this distribution.")
+
+    def log_prob(self, value):
+        """Compute the log probability for a given value."""
+        if hasattr(self.custom_dist, 'log_prob'):
+            return self.custom_dist.log_prob(value)
+        elif hasattr(self.custom_dist, 'log_likelihood'):
+            return self.custom_dist.log_likelihood(value)
+        elif hasattr(self.custom_dist, 'logpdf'):
+            # Scipy distributions have logpdf method
+            return self.custom_dist.logpdf(value)
+        raise NotImplementedError("Log probability method is not implemented for this distribution.")
+
+    def rsample(self, sample_shape=torch.Size()):
+        """Generate reparameterized samples from the custom distribution."""
+        if hasattr(self.custom_dist, 'rsample'):
+            return self.custom_dist.rsample(sample_shape)
+        elif hasattr(self.custom_dist, 'sample'):
+            # Fallback to sample if rsample is not implemented
+            return self.custom_dist.sample(sample_shape)
+        raise NotImplementedError("Sampling method is not implemented for this distribution.")
+
+    def sample(self, sample_shape=torch.Size()):
+        """Generate samples from the custom distribution (non-differentiable if using sample)."""
+        if hasattr(self.custom_dist, 'sample'):
+            return self.custom_dist.sample(sample_shape)
+        elif hasattr(self.custom_dist, 'rvs'):
+            # Scipy distributions have rvs method
+            return self.custom_dist.rvs(size=sample_shape.numel())
+        raise NotImplementedError("Sample method is not implemented for this distribution.")
+
+    def __repr__(self):
+        return f"TorchDistributionWrapper({self.custom_dist})"
